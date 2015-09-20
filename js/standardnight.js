32a7e6568a1b8b3e1aa4f979c367787d79d23315
Figgynewts <znewton@iastate.edu>
2015-09-20 02:50:38 -0500

    creation of outline for standard night operations

---
+++ js/standardnight.js
@@ -0,0 +1,192 @@
+var main = function(){
+	
+};
+
+for(var x=0;x<roleData.length;x++){
+	if(roleData[x].pName === "Seer"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "P.I."){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Aura Seer"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Sorceress"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Troublemaker"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Body Guard"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Wolves"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Vampires"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Witch"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Priest"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Old Hag"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	} else if(roleData[x].pName === "Cult Leader"){
+		//display description
+		if(roleData[x].alive == true){
+			//display alive
+		} else{
+			//display dead
+		}
+		for(var y=0;y<roleData.length;y++){
+			if(roleData[x].alive == true){
+				//append Name Target button
+			}
+			$('#buttonClickID').click(function(){
+				//display role team (villager or Werewolf)
+			});
+		}
+	}
+}
+
+
+var list=localStorage.getItem('list');
+var roleData=[];
+$(document).ready(main);
