1d625169ec73305b4fdc88e4a305b50a5025de4a
Figgynewts <znewton@iastate.edu>
2015-09-19 21:07:37 -0500

    added local storage functionalities to setup.js and removed werewolf clock

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,39 @@
 var main = function(){
-	
+	setup();
 };
 
 /*Game setup*/
+var setup = function(){
+	$('#quickSetup').click(function(){
+		quick=true;
+		custom=false;
+	});
+	$('#customSetup').click(function(){
+		quick=false;
+		custom=true;
+	});
+	
+	if(custom){			//custom setup functions
+		load();
+		$('#playerNumForm').submit(function(event){
+			console.log('player num submitted'); //debug player num submission
+			
+			load();
+			pNum=$('input:first').val();
+			save();
+			$('#playerNumForm').toggleClass('.hidden');
+			event.preventDefault();
+		});
+		
+		var compileRoleList = function(){
+			
+		};
+		
+		save();
+	}else if(quick){	//quick setup functions
+		
+	}
+};
 
 /*Sign in Sheet*/
 var signIn = function(){
@@ -12,9 +43,25 @@ var signIn = function(){
 /*Local Storage Functions*/
 var load = function(){
 	list=JSON.parse(localStorage.getItem('list'));
+	dayLength=JSON.parse(localStorage.getItem('dayLength'));
+	pNum=JSON.parse(localStorage.getItem('pNum'));
+	pName=JSON.parse(localStorage.getItem('pName'));
+	tempRole=JSON.parse(localStorage.getItem('tempRole'));
+	quick=JSON.parse(localStorage.getItem('quick'));
+	custom=JSON.parse(localStorage.getItem('custom'));
+	customResult=JSON.parse(localStorage.getItem('customResult'));
+	roleNumList=JSON.parse(localStorage.getItem('roleNumList'));
 }
 var save = function(){
 	localStorage.setItem("list", JSON.stringify(list));
+	localStorage.setItem("dayLength", JSON.stringify(dayLength));
+	localStorage.setItem("pNum", JSON.stringify(pNum));
+	localStorage.setItem("pName", JSON.stringify(pName));
+	localStorage.setItem("tempRole", JSON.stringify(tempRole));
+	localStorage.setItem("quick", JSON.stringify(quick));
+	localStorage.setItem("custom", JSON.stringify(custom));
+	localStorage.setItem("customResult", JSON.stringify(customResult));
+	localStorage.setItem("roleNumList", JSON.stringify(roleNumList));
 }
 var reset = function(){
 	localStorage.clear();
@@ -24,10 +71,15 @@ var reset = function(){
 }
 
 /*Initialize Variables*/
+
 var list=[];
-var dayLength;
-var pNum;
-var pName;
-var role;
+var dayLength=0;
+var pNum=0;
+var pName="";
+var tempRole="";
+var quick=false;
+var custom=true;
+var customResult=Object();
+var roleNumList=[];
 
 $(document).ready(main);
